a620dadeec2ede2a374aa72f586805750aa02e33
Adrián Octavio Terrazas García <adriantega12@gmail.com>
2018-09-11 19:06:09 -0500

    Initiate routing

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express'),
+	app = express();
+
+// USERS
+
+
+// PUBLICATIONS
+
+
+// COMMENTS
+
+
+// MESSAGES
+
+
+app.listen( 3000, () => console.log( 'Started listening on port 3000!' ) );
