a7ae327d36a9957070ca2b8c6fdb6f8376da833c
Adrián Octavio Terrazas García <tavo_terrazasg@otmail.com>
2018-09-17 00:54:58 -0500

    Add comments router to app.js

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Router requires
+const commentsRoutes = require('./routes/comments.js');
+
 // USERS
 
 
@@ -13,6 +16,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 // COMMENTS
+app.use('/users/:userId/posts/:postId', commentsRoutes);
+
+// User and Comments
+// INDEX
+app.get('/users/:userId/comments', (req, res) => {
+	res.send('INDEX');
+});
 
 
 // MESSAGES
