e46b6605b26d5a51074c520dbf0ee68a3f15f96a
Adrián Octavio Terrazas García <tavo_terrazasg@otmail.com>
2018-09-16 20:46:57 -0500

    First draft of Posts routes without JSON response

--- app.js
+++ app.js
@@ -2,16 +2,39 @@ const express = require('express');
 
 const app = express();
 
-// USERS
+// Users
 
 
-// PUBLICATIONS
+// Posts
+// INDEX Post
+app.get('/posts', (req, res) => {
+	
+});
+
+// NEW Post
+app.post('/users/:userId/posts', (req, res) => {
+
+});
+
+// SHOW Post
+app.get('/users/:userId/posts/:postId', (req, res) => {
+
+});
+
+// UPDATE Post
+app.put('/users/:userId/posts/:postId', (req, res) => {
+
+});
+
+// DESTROY Post
+app.delete('/users/:userId/posts/:postId', (req, res) => {
+
+});
+
+// Comments
 
 
-// COMMENTS
-
-
-// MESSAGES
+// Messages
 
 
 app.listen(3000, () => console.log('Started listening on port 3000!'));
