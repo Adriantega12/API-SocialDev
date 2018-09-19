4a9f5be7c638e831994371d793c4f665683b30d1
Adrián Octavio Terrazas García <tavo_terrazasg@otmail.com>
2018-09-18 23:05:36 -0500

    Structured routes

--- app.js
+++ app.js
@@ -2,26 +2,7 @@ const express = require('express');
 const router = require('./routes');
 
 const app = express();
-/*
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
-// Router requires
-const postsRoutes = require('./routes/posts.js');
-const messagesRoutes = require('./routes/messages.js');
-*/
-
-// Users
-
-
-// Posts
-app.use('/users/:userId/posts', postsRoutes);
-
-// Comments
-
-
-// Messages
-app.use('/users/:userId/messages', commentsRoutes);
-
+app.use(router);
 
 app.listen(3000, () => console.log('Started listening on port 3000!'));
--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,13 @@ const { Router } = require('express');
 const router = Router();
 const bodyParser = require('body-parser');
 
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
+
+const postsRoutes = require('./posts');
+const messagesRoutes = require('./messages');
+
+router.use('/users/:userId/posts', postsRoutes);
+router.use('/users/:userId/messages', messagesRoutes);
+
 module.exports = router;
