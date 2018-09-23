6a94f9af6c473e996bbdad10295eaf4f3898a118
Adrián Octavio Terrazas García <tavo_terrazasg@otmail.com>
2018-09-23 15:25:20 -0500

    Add emails routes

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,7 @@ const usersRoutes = require('./users');
 const postsRoutes = require('./posts');
 const commentsRoutes = require('./comments');
 const messagesRoutes = require('./messages');
+const emailsRoutes = require('./emails');
 
 // Test
 router.get('/', (req, res) => { res.send('Principal'); });
@@ -27,5 +28,8 @@ router.use('/posts/:postId/comments', commentsRoutes);
 
 // Messages
 router.use('/users/:userId/messages', messagesRoutes);
+
+// Emails
+router.use('/users/:userId/emails', emailsRoutes);
 
 module.exports = router;
