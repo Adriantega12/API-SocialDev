03c9374cdf63edc33cabc50b4be7a8b4b5733214
Adrián Octavio Terrazas García <tavo_terrazasg@otmail.com>
2018-09-23 15:30:09 -0500

    Add dotenv to package.json for future use. Modified app.js for it.

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = require('./routes');
+require('dotenv').config();
 
 const app = express();
 
 app.use(router);
 
-app.listen(3000, () => console.log('Started listening on port 3000!'));
+app.listen(process.env.PORT, () => console.log(`Started listening on port ${process.env.PORT}!`));
