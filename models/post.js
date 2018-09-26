9deff729017925a4b865fbb9a86c169242886f0e
Diego <mega-311@hotmail.com>
2018-09-26 17:40:54 -0500

    Create Post Model and added to index

---
+++ models/post.js
@@ -0,0 +1,72 @@
+
+class Post {
+  /**
+   * Constructor for class User.
+   * @param  {number} id           Unique value identifies a unique post.
+   * @param  {number} authorId     Unique value identifies a unique user.
+   * @param  {number} title        Title of post.
+   * @param  {number} text         Body of post.
+   * @param  {number} date         Creation date.
+   * @param  {number} score        Post score.
+   */
+  constructor(...args) {
+    [
+      this.id,
+      this.authorId,
+      this.title,
+      this.text,
+      this.date,
+      this.score,
+    ] = args;
+  }
+
+  set id(value) {
+    this.id = value;
+  }
+
+  set authorId(value) {
+    this.authorId = value;
+  }
+
+  set title(value) {
+    this.title = value;
+  }
+
+  set text(value) {
+    this.text = value;
+  }
+
+  set date(value) {
+    this.date = value;
+  }
+
+  set score(value) {
+    this.score = value;
+  }
+
+  get id() {
+    return this.id;
+  }
+
+  get authorId() {
+    return this.authorId;
+  }
+
+  get title() {
+    return this.title;
+  }
+
+  get text() {
+    return this.text;
+  }
+
+  get date() {
+    return this.date;
+  }
+
+  get score() {
+    return this.score
+  }
+}
+
+module.exports = Post;
