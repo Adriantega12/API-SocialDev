b3525996e63aaf7022dbaf59d558dd3583e2542f
Adrián Octavio Terrazas García <tavo_terrazasg@hotmail.com>
2018-09-25 19:56:54 -0500

    Made user constructor

--- models/user.js
+++ models/user.js
@@ -1,1 +1,35 @@
+class User {
+  /**
+   * Constructor for class User.
+   * @param  {number} id           [description]
+   * @param  {number} roleId       [description]
+   * @param  {string} email        [description]
+   * @param  {string} password     [description]
+   * @param  {string} passwordSalt [description]
+   * @param  {string} passwordHash [description]
+   * @param  {string} githubToken  [description]
+   * @param  {string} firstName    [description]
+   * @param  {string} lastName     [description]
+   * @param  {number} age          [description]
+   * @param  {number} level        [description]
+   * @param  {blob} profilePic   [description]
+   * @return {User}              [description]
+   */
+  constructor(...args) {
+    this.id = id;
+    this.roleId = roleId;
+    this.email = email;
+    this.password = password;
+    this.passwordSalt = passwordSalt;
+    this.passwordHash = passwordHash;
+    this.githubToken = githubToken;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.level = level;
+    this.profilePic = profilePic;
+    console.log(args);
+  }
+}
 
+module.exports = User();
