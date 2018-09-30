96b47c72fedc7d214437fde6ce234cab4217a619
Adrián Octavio Terrazas García <tavo_terrazasg@hotmail.com>
2018-09-29 23:46:23 -0500

    Add mini ORM Attachment Model

--- models/attachment.js
+++ models/attachment.js
@@ -1,3 +1,5 @@
+const db = require('../db');
+
 class Attachment {
   /**
    * Constructor for class Attachment.
@@ -14,6 +16,56 @@ class Attachment {
     this.id = id;
     this.postId = postId;
     this.data = data;
+  }
+
+  static async getAll() {
+    const data = await db.getAll('attachments');
+    const response = [];
+    data.forEach((row) => {
+      response.push(new Attachment(row));
+    });
+    return response;
+  }
+
+  static async get(attachmentId) {
+    const data = await db.get('attachments', '*', attachmentId);
+    return data.length !== 0 ? new Attachment(data[0]) : data;
+  }
+
+  static async insert(attachment) {
+    let id;
+    try {
+      const response = await db.insert('attachments', attachment);
+      id = response.insertId;
+    } catch (error) {
+      return error;
+    }
+
+    return id > 0 ? new Attachment({ id, ...attachment }) : [];
+  }
+
+  async update(keyVals) {
+    let updatedRows;
+    try {
+      const results = await db.update('attachments', keyVals, this.id);
+      updatedRows = results.affectedRows;
+    } catch (error) {
+      return error;
+    }
+
+    return updatedRows > 0;
+  }
+
+  static async delete(attachmentId) {
+    let deletedRows;
+    try {
+      const results = await db.delete('attachments', attachmentId);
+      deletedRows = results.affectedRows;
+    } catch (error) {
+      return error;
+    }
+
+    return deletedRows > 0;
   }
 }
 
