4070f8b03423acf5258311121742235b0bc0cd7f
Adrián Octavio Terrazas García <tavo_terrazasg@hotmail.com>
2018-09-29 23:42:27 -0500

    Add mini ORM to Friendship Model

--- models/friendship.js
+++ models/friendship.js
@@ -1,3 +1,5 @@
+const db = require('../db');
+
 class Friendship {
   /**
    * Constructor for class Friendship.
@@ -23,6 +25,56 @@ class Friendship {
     this.lastActionId = lastActionId;
     this.date = date;
     this.status = status;
+  }
+
+  static async getAll() {
+    const data = await db.getAll('friendships');
+    const response = [];
+    data.forEach((row) => {
+      response.push(new Friendship(row));
+    });
+    return response;
+  }
+
+  static async get(friendshipId) {
+    const data = await db.get('friendships', '*', friendshipId);
+    return data.length !== 0 ? new Friendship(data[0]) : data;
+  }
+
+  static async insert(user) {
+    let id;
+    try {
+      const response = await db.insert('friendships', user);
+      id = response.insertId;
+    } catch (error) {
+      return error;
+    }
+
+    return id > 0 ? new Friendship({ id, ...user }) : [];
+  }
+
+  async update(keyVals) {
+    let updatedRows;
+    try {
+      const results = await db.update('friendships', keyVals, this.id);
+      updatedRows = results.affectedRows;
+    } catch (error) {
+      return error;
+    }
+
+    return updatedRows > 0;
+  }
+
+  static async delete(friendshipId) {
+    let deletedRows;
+    try {
+      const results = await db.delete('friendships', friendshipId);
+      deletedRows = results.affectedRows;
+    } catch (error) {
+      return error;
+    }
+
+    return deletedRows > 0;
   }
 }
 
