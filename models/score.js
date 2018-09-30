35827c87a4fb924659e2598e2e93376f64abff05
Adrián Octavio Terrazas García <tavo_terrazasg@hotmail.com>
2018-09-29 23:44:41 -0500

    Add mini ORM to Score Model

--- models/score.js
+++ models/score.js
@@ -1,3 +1,5 @@
+const db = require('../db');
+
 class Score {
   /**
    * Constructor for class Score.
@@ -20,6 +22,56 @@ class Score {
     this.userId = userId;
     this.score = score;
     this.date = date;
+  }
+
+  static async getAll() {
+    const data = await db.getAll('scores');
+    const response = [];
+    data.forEach((row) => {
+      response.push(new Score(row));
+    });
+    return response;
+  }
+
+  static async get(scoreId) {
+    const data = await db.get('scores', '*', scoreId);
+    return data.length !== 0 ? new Score(data[0]) : data;
+  }
+
+  static async insert(score) {
+    let id;
+    try {
+      const response = await db.insert('scores', score);
+      id = response.insertId;
+    } catch (error) {
+      return error;
+    }
+
+    return id > 0 ? new Score({ id, ...score }) : [];
+  }
+
+  async update(keyVals) {
+    let updatedRows;
+    try {
+      const results = await db.update('scores', keyVals, this.id);
+      updatedRows = results.affectedRows;
+    } catch (error) {
+      return error;
+    }
+
+    return updatedRows > 0;
+  }
+
+  static async delete(scoreId) {
+    let deletedRows;
+    try {
+      const results = await db.delete('scores', scoreId);
+      deletedRows = results.affectedRows;
+    } catch (error) {
+      return error;
+    }
+
+    return deletedRows > 0;
   }
 }
 
