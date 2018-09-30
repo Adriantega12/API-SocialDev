6e8a87b1a1ddcb9d5e17010e77cad6fc49513224
Adrián Octavio Terrazas García <tavo_terrazasg@hotmail.com>
2018-09-29 20:51:42 -0500

    Add mini ORM in Post Model

--- models/post.js
+++ models/post.js
@@ -1,9 +1,10 @@
+const db = require('../db');
 
 class Post {
   /**
    * Constructor for class Post.
    * @param  {number} id       Unique value, identifies a unique post.
-   * @param  {number} authorId Identifier of the user that originally wrote the post.
+   * @param  {number} authorId Identifier of the post that originally wrote the post.
    * @param  {string} title    Title of the post.
    * @param  {string} text     Body of the post.
    * @param  {Date} date       Creation date.
@@ -24,6 +25,56 @@ class Post {
     this.text = text;
     this.date = date;
     this.score = score;
+  }
+
+  static async getAll() {
+    const data = await db.getAll('posts');
+    const response = [];
+    data.forEach((row) => {
+      response.push(new Post(row));
+    });
+    return response;
+  }
+
+  static async get(postId) {
+    const data = await db.get('posts', '*', postId);
+    return data.length !== 0 ? new Post(data[0]) : data;
+  }
+
+  static async insert(post) {
+    let id;
+    try {
+      const response = await db.insert('posts', post);
+      id = response.insertId;
+    } catch (error) {
+      return error;
+    }
+
+    return id > 0 ? new Post({ id, ...post }) : [];
+  }
+
+  async update(keyVals) {
+    let updatedRows;
+    try {
+      const results = await db.update('posts', keyVals, this.id);
+      updatedRows = results.affectedRows;
+    } catch (error) {
+      return error;
+    }
+
+    return updatedRows > 0;
+  }
+
+  static async delete(postId) {
+    let deletedRows;
+    try {
+      const results = await db.delete('posts', postId);
+      deletedRows = results.affectedRows;
+    } catch (error) {
+      return error;
+    }
+
+    return deletedRows > 0;
   }
 }
 
