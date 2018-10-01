4f01e9271f78d377b4b835af089d234a20c8d8ab
Adrián Octavio Terrazas García <tavo_terrazasg@hotmail.com>
2018-09-30 23:00:03 -0500

    Add validate method (Should have something added later)

--- middlewares/validator.js
+++ middlewares/validator.js
@@ -28,6 +28,22 @@ class Validator {
       status: 409,
       details: {},
     };
+
+    for (let part in rules) { // part = body, params
+      for (let field in rules[part]) { // field = body { attrib }
+        let validators = rules[part][field].split(' '); // validator = body { attrib: 'validator' }
+        validators.forEach((f) => { // validator = [ 'required', 'word' ]
+          if (!Validator[f](req[part][field] || '')) { //
+            if (Array.isArray(error.details[field])) {
+              error.details[field].push(`The field ${field} should be a valid ${f}`);
+            } else {
+              error.details[field] = [`The field ${field} should be a valid ${f}`];
+            }
+          }
+        });
+      }
+    }
+    Object.keys(error.details).length ? next(error) : next();
   }
 }
 
