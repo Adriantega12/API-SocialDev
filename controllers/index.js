465cafe4854f46d472c45c148125a4e7daf15001
Diego <mega-311@hotmail.com>
2018-10-01 19:59:08 -0500

    Email controller created and added to index

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,12 @@ const usersController = require('./users');
 const postsController = require('./posts');
 const commentsController = require('./comments');
 const messagesController = require('./messages');
+const emailsController = require('./emails');
 
 module.exports = {
   usersController,
   postsController,
   commentsController,
   messagesController,
+  emailsController,
 };
