c24a72573dc374d1213f65d38685d2891373ef02
Adrián Octavio Terrazas García <tavo_terrazasg@hotmail.com>
2018-10-01 22:33:41 -0500

    Add middleware for scores

--- controllers/scores.js
+++ controllers/scores.js
@@ -10,9 +10,14 @@ class ScoresController {
     this.delete = this.delete.bind(this);
   }
 
-  // ***Needs reviewing***
-  async getAll(req, res) {
-    const data = await Score.getAll();
+  async getAll(req, res, next) {
+    let data;
+
+    try {
+      data = await Score.getAll();
+    } catch (error) {
+      next(error);
+    }
 
     const json = {
       data: data,
@@ -30,8 +35,14 @@ class ScoresController {
     res.send(json);
   }
 
-  async get(req, res) {
-    const data = await Score.get(req.params.scoreId);
+  async get(req, res, next) {
+    let data;
+
+    try {
+      data = await Score.get(req.params.scoreId);
+    } catch (error) {
+      next(error);
+    }
 
     if (data.length === 0) {
       res.status(404); // Not Found
@@ -42,8 +53,14 @@ class ScoresController {
     res.send(data);
   }
 
-  async insert(req, res) {
-    const data = await Score.insert(req.body);
+  async insert(req, res, next) {
+    let data;
+
+    try {
+      data = await Score.insert(req.body);
+    } catch (error) {
+      next(error);
+    }
 
     if (data.length === 0) {
       res.status(409); // Conflict
@@ -54,14 +71,21 @@ class ScoresController {
     res.send(data);
   }
 
-  async update(req, res) {
-    const data = await Score.get(req.params.scoreId);
+  async update(req, res, next) {
+    let data;
+
+    try {
+      data = await Score.get(req.params.scoreId);
+    } catch (error) {
+      next(error);
+    }
 
     if (data.length === 0) {
       res.status(404).send(data); // Not Found
     }
 
     const updated = await data.update(req.body);
+    data = new Score(req.body);
 
     if (updated) {
       res.status(200); // OK
@@ -72,8 +96,14 @@ class ScoresController {
     res.send(data);
   }
 
-  async delete(req, res) {
-    const deleted = await Score.delete(req.params.scoreId);
+  async delete(req, res, next) {
+    let deleted;
+
+    try {
+      deleted = await Score.delete(req.params.scoreId);
+    } catch (error) {
+      next(error);
+    }
 
     if (deleted) {
       res.status(200); // OK
--- routes/scores.js
+++ routes/scores.js
@@ -1,28 +1,53 @@
 const router = require('express').Router();
+const { scoresController } = require('../controllers');
+const { validator } = require('../middlewares');
 
 // INDEX Scores
-router.get('/', (req, res) => {
-  res.send('INDEX');
-});
+router.get('/', scoresController.getAll);
 
 // NEW Score
-router.post('/', (req, res) => {
-  res.send('NEW');
-});
+router.post('/', (req, res, next) => {
+  validator.validate(req, res, next, {
+    body: {
+      postsId: 'required integer',
+      userId: 'required integer',
+      score: 'required integer',
+      date: 'required date',
+    },
+  });
+}, scoresController.insert);
 
 // SHOW Score
-router.get('/:scoreId', (req, res) => {
-  res.send('SHOW');
-});
+router.get('/:scoreId', (req, res, next) => {
+  validator.validate(req, res, next, {
+    params: {
+      scoreId: 'integer',
+    },
+  });
+}, scoresController.get);
 
 // UPDATE Score
-router.put('/:scoreId', (req, res) => {
-  res.send('UPDATE');
-});
+router.put('/:scoreId', (req, res, next) => {
+  validator.validate(req, res, next, {
+    params: {
+      scoreId: 'integer',
+    },
+    body: {
+      postsId: 'integer',
+      userId: 'integer',
+      score: 'integer',
+      date: 'date',
+    },
+  });
+}, scoresController.update);
 
 // DESTROY Score
-router.delete('/:scoreId', (req, res) => {
-  res.send('DELETE');
-});
+router.delete('/:scoreId', (req, res, next) => {
+  validator.validate(req, res, next, {
+    params: {
+      scoreId: 'integer',
+    },
+  });
+}, scoresController.delete);
 
 module.exports = router;
