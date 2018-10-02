17d06db48e89ca1f4420ddb64f9fb998ea6c34f3
Diego <mega-311@hotmail.com>
2018-10-02 00:51:02 -0500

    Added to  Factory

--- factory/index.js
+++ factory/index.js
@@ -1,6 +1,10 @@
 exports.User = require('./user');
 exports.Post = require('./post');
 exports.Comment = require('./comment');
+exports.Message = require('./message');
+exports.Score = require('./score');
+exports.Email = require('./email');
+exports.Friendship = require('./friendship');
 
 for (var i = 0; i < 100; i++) {
   User.insert({
@@ -34,6 +38,49 @@ for (var i = 0; i < 100; i++) {
       authorId: i,
       date: '2018-10-01 05:40:06',
       content: 'lorem',
-      isEdited: true,
+      isEdited: false,
+  });
+}
+
+for (var i = 0; i < 100; i++) {
+  Message.insert({
+      senderId: i,
+      receiverId: i,
+      text: 'lorem',
+      date: '2018-10-01 05:40:06',
+  });
+}
+
+for (var i = 0; i < 100; i++) {
+  Score.insert({
+      postId: i,
+      userId: i,
+      score: 0,
+      date: '2018-10-01 05:40:06',
+  });
+}
+
+for (var i = 0; i < 100; i++) {
+  Email.insert({
+      userId: i,
+      email: String.valueOf(i) + '@' + String.valueOf(i) + '.com',
+  });
+}
+
+for (var i = 0; i < 100; i++) {
+  Friendship.insert({
+      userOneId: i,
+      userTwoId: i,
+      lastActionId: 0,
+      date: '2018-10-01 05:40:06',
+      status: 0,
+  });
+}
+
+for (var i = 0; i < 100; i++) {
+  Friendship.insert({
+      postId: i,
+      data: null,
+
   });
 }
--- models/attachment.js
+++ models/attachment.js
@@ -5,7 +5,7 @@ class Attachment {
    * Constructor for class Attachment.
    * @param  {number}     id     Identifier of attachment.
    * @param  {number}     postId Identifier of the post the attachment was posted to.
-   * @param  {date}       data   File that the attachment uses.
+   * @param  {blob}       data   File that the attachment uses.
    * @return {Attachment}        New instance of an Attachment.
    */
   constructor({
