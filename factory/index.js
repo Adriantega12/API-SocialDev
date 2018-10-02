a7146566f34c3d988b1b51351a94f6c059f7ba9f
Marcela Bitar <marcela.bitar.h@live.com>
2018-10-02 01:36:31 -0500

    Se modificaron atributos para el factory de role

---
+++ factory/index.js
@@ -0,0 +1,95 @@
+exports.User = require('./user');
+exports.Post = require('./post');
+exports.Comment = require('./comment');
+exports.Message = require('./message');
+exports.Score = require('./score');
+exports.Email = require('./email');
+exports.Friendship = require('./friendship');
+exports.Attachment = require('./attachment');
+exports.Role = require('./role')
+
+for (var i = 0; i < 100; i++) {
+  User.insert({
+      roleId: i,
+      email: String.valueOf(i) + '@' + String.valueOf(i) + '.com',
+      password: i,
+      passwordSalt: String.valueOf(i),
+      passwordHash: 'SHA2',
+      githubToken: String.valueOf(i),
+      firstName: String.valueOf(i),
+      lastName: String.valueOf(i),
+      age: 20,
+      level: 1,
+      profilePic: null,
+  });
+}
+
+for (var i = 0; i < 100; i++) {
+  Post.insert({
+      authorId: i,
+      title: 'TITLE' + String.valueOf(i),
+      text: 'lorem',
+      date: '2018-10-01 05:40:06',
+      score: 0,
+  });
+}
+
+for (var i = 0; i < 100; i++) {
+  Comment.insert({
+      postId: i,
+      authorId: i,
+      date: '2018-10-01 05:40:06',
+      content: 'lorem',
+      isEdited: false,
+  });
+}
+
+for (var i = 0; i < 100; i++) {
+  Message.insert({
+      senderId: i,
+      receiverId: i,
+      text: 'lorem',
+      date: '2018-10-01 05:40:06',
+  });
+}
+
+for (var i = 0; i < 100; i++) {
+  Score.insert({
+      postId: i,
+      userId: i,
+      score: 0,
+      date: '2018-10-01 05:40:06',
+  });
+}
+
+for (var i = 0; i < 100; i++) {
+  Email.insert({
+      userId: i,
+      email: String.valueOf(i) + '@' + String.valueOf(i) + '.com',
+  });
+}
+
+for (var i = 0; i < 100; i++) {
+  Friendship.insert({
+      userOneId: i,
+      userTwoId: i,
+      lastActionId: 0,
+      date: '2018-10-01 05:40:06',
+      status: 0,
+  });
+}
+
+for (var i = 0; i < 100; i++) {
+  Attachment.insert({
+      postId: i,
+      data: null,
+
+  });
+}
+
+for (var i = 0; i < 100; i++) {
+  Role.insert({
+      roleId: i,
+      name: String.valueOf(i),
+  });
+}
