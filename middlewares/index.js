2c9048018b73c0b1314fc258551688bb45823806
Adrián Octavio Terrazas García <tavo_terrazasg@hotmail.com>
2018-10-11 23:50:52 -0500

    Moved errorHandler to it's own file and added Datetime handle class

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,6 +1,2 @@
 exports.validator = require('./validator');
-
-exports.errorHandler = function errorHandler(err, req, res, next) {
-  console.error('Error handler', err);
-  return res.status(err.status || 500).send(err);
-};
+exports.errorHandler = require('./errorHandler');
