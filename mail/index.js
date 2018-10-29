bfea6eac43d2fc4fbaa2f7653ed158ebd3f88526
Adrián Octavio Terrazas García <tavo_terrazasg@hotmail.com>
2018-10-29 17:34:48 -0600

    Cleanup and .sample-env edit

--- mail/index.js
+++ mail/index.js
@@ -20,53 +20,11 @@ class Mailer {
     this.mailOptions = {
       from: process.env.MAIL_USER,
     };
-
-    // Ethereal
-    /*
-    this.transporter = nodemailer.createTransport({
-      host: process.env.MAILER_HOST,
-      port: process.env.MAILER_PORT,
-      secure: true,
-      auth: {
-        user: process.env.MAILER_USER,
-        pass: process.env.MAILER_PASS,
-      },
-    });
-    */
-
-    // Gmail
-    //console.log(this.oauth2Client.refreshAccessToken().then(res => console.log('res')));
-    /*const promise = new Promise((resolve, reject) => {
-      console.log(this.oauth2Client.refreshAccessToken()
-        .then(res => resolve(res.credentials.accessToken)));
-    });*/
-
-    /*
-    console.log(this.accessToken);
-    this.accessToken = new Promise(async (resolve, reject) => {
-      await this.oauth2Client.refreshAccessToken()
-        .then(res => resolve(res.credentials.accessToken));
-    });
-    console.log(this.accessToken);
-
-    this.transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        type: 'OAuth2',
-        user: process.env.MAIL_USER,
-        clientId: process.env.CLIENT_ID,
-        clientSecret: process.env.CLIENT_SECRET,
-        refreshToken: process.env.REFRESH_TOKEN,
-        accessToken: this.accessToken,
-      },
-    });
-    */
   }
 
   async init() {
     this.accessToken = await this.oauth2Client.refreshAccessToken()
       .then(res => res.credentials.access_token);
-    console.log(this.accessToken);
     this.transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
@@ -81,7 +39,6 @@ class Mailer {
         rejectUnauthorized: false,
       },
     });
-    console.log('Bitch');
   }
 
   async sendMail(options) {
@@ -95,10 +52,6 @@ class Mailer {
         if (error) {
           return reject(error);
         }
-
-        console.log('Message sent: %s', info.messageId);
-        // Preview only available when sending through an Ethereal account
-        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
         return resolve(info);
       });
     });
