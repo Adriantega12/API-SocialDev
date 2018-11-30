fcc27280bba1e8507f84c63d0ffd7257b0d54cde
Adrián Octavio Terrazas García <tavo_terrazasg@hotmail.com>
2018-11-29 23:06:41 -0600

    Defined as static repo uploads dir

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 require('@google-cloud/debug-agent').start();
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const router = require('./routes');
@@ -14,6 +15,9 @@ const app = express();
 // Cargar middlewares
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Servir contenido estático
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // CORS
 app.use(cors());
